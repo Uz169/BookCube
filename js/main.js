e56adcce29ce63541265cb522e3e1e11dc3962d0
Uz1 <115734822+Uz169@users.noreply.github.com>
2023-11-03 22:44:19 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/*price range*/
+
+ $('#sl2').slider();
+
+	var RGBChange = function() {
+	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+	};	
+		
+/*scroll to top*/
+
+$(document).ready(function(){
+	$(function () {
+		$.scrollUp({
+	        scrollName: 'scrollUp', // Element ID
+	        scrollDistance: 300, // Distance from top/bottom before showing element (px)
+	        scrollFrom: 'top', // 'top' or 'bottom'
+	        scrollSpeed: 300, // Speed back to top (ms)
+	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
+	        animation: 'fade', // Fade, slide, none
+	        animationSpeed: 200, // Animation in speed (ms)
+	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
+					//scrollTarget: false, // Set a custom target element for scrolling to the top
+	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element, can contain HTML
+	        scrollTitle: false, // Set a custom <a> title if required.
+	        scrollImg: false, // Set true to use image
+	        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+	        zIndex: 2147483647 // Z-Index for the overlay
+		});
+	});
+});
+
+
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("logout-link").addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+
+        // Perform a logout by making an AJAX request to logout.php
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "logout.php", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+
+                window.location.href = "login.php";
+            }
+        };
+        xhr.send();
+    });
+});
+</script>
